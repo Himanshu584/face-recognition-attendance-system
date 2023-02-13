ed3e6adafcf93dc93d561e371d35a8fa4b67aefe
Himanshu Sharma <Hsharma98765@gmail.com>
2023-02-13 17:42:11 +0530

    match index fetching

--- main.py
+++ main.py
@@ -28,9 +28,23 @@ while True:
     
     simg = cv2.resize(img,(0,0),None,0.25,0.25) # rescale the image to be computationally comfortable
     simg = cv2.cvtColor(simg,cv2.COLOR_BGR2RGB)
-    
+
+    # get current face from simg
+    currface = face_recognition.face_locations(simg)
+    currfaceencoding = face_recognition.face_encodings(simg,currface) # get encoding for only current face in simg
+
+    # display graphics
     background[110:110+288,100:100+352] = img # set the video frame from webcam onto bg
     background[0:0+500,550:550+444] = modelist[1]
+
+    # compare faces with face encodings and get most probable face 
+    for faceencoding,currfacelocation in zip(currfaceencoding,currface):
+        matches = face_recognition.compare_faces(encodingslist,faceencoding)
+        facedis = face_recognition.face_distance(encodingslist,faceencoding)
+
+        matchindex = np.argmin(facedis)
+        print(matchindex)
+        
 
 
     cv2.imshow("Attendance System",background)
