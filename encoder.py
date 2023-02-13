3310f5f6a884ef280477bc37adef7eab1628f3a2
Himanshu Sharma <Hsharma98765@gmail.com>
2023-02-13 17:27:05 +0530

    added modes, images, created encodings

---
+++ encoder.py
@@ -0,0 +1,35 @@
+import cv2
+import face_recognition
+import os
+import pickle
+
+# load student images 
+imgdir = "images"
+studentimglist = []
+studentimgids = []
+
+# get all images of students into studentimglist
+for img_pth in os.listdir(imgdir):
+    studentimglist.append(cv2.imread(os.path.join(imgdir,img_pth)))
+    studentimgids.append(img_pth.split('.')[0]) # get relative ids of students into studentimgids
+
+# encoding generator function
+def encodeimages(studentimages):
+    faceencodinglist = []
+    for img in studentimages:
+        student = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)  # convert bgr to rbg
+        encoding = face_recognition.face_encodings(student)[0] # encoding of single face
+        faceencodinglist.append(encoding)
+    return faceencodinglist
+
+
+# calling encoding generator function
+face_encodings = encodeimages(studentimglist)
+encodingswithids = [face_encodings,studentimgids]
+
+
+# dump all the encodings with ids into pickle/pascal file
+with open("encodings.p",'wb') as f:
+    pickle.dump(encodingswithids,f)
+
+    
--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import cv2
-import dlib
+import os
+import pickle 
+import numpy as np
+import face_recognition
 
+# get video from the webcam
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) 
-cap.set(3,1280)
-cap.set(4,720)
+cap.set(3,550) # set width of videocapture
+cap.set(4,250) # set height of videocapture
 
+
+background = cv2.imread("resources/BASE.png")  # background design
+modes_folder = os.listdir("resources/modes") # mode path
+modelist = []
+# get all modes into model
+for file in modes_folder:
+    modelist.append(cv2.imread("resources/modes/" + file))
+
+# load the encodings file 
+with open("encodings.p","rb") as f:
+    encodingwithids = pickle.load(f)
+
+encodingslist, encodingids = encodingwithids
 
 while True: 
     success, img = cap.read() 
-    cv2.imshow("Attendance System",img)
+    
+    simg = cv2.resize(img,(0,0),None,0.25,0.25) # rescale the image to be computationally comfortable
+    simg = cv2.cvtColor(simg,cv2.COLOR_BGR2RGB)
+    
+    background[110:110+288,100:100+352] = img # set the video frame from webcam onto bg
+    background[0:0+500,550:550+444] = modelist[1]
+
+
+    cv2.imshow("Attendance System",background)
     cv2.waitKey(1)
     if cv2.getWindowProperty("Attendance System",cv2.WND_PROP_VISIBLE) <1:
         break
+    
 cap.release()
 cv2.destroyAllWindows()
+
+    
 
